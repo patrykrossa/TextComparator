375ff5db6e3b905c32df29c126992a62a54c73e3
WikarNotAvailable <wikar8998@gmail.com>
2022-11-14 13:25:15 +0100

    changed web api project

---
+++ API/TextComparatorAPI/WebApi/Controllers/ComparatorController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace WebApi.Controllers
+{
+    public class ComparatorController : Controller
+    {
+        [HttpPost]
+        public async IActionResult UploadFile()
+        {
+
+        }
+    }
+}
